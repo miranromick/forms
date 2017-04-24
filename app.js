58e0f98c4a1771d66b7322cc25fa7e1a792a7fe6
learn <learn@learn18.local>
2017-04-24 16:37:55 -0700

    v1

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var fs = require('fs')
+var teamCreated = false;
+app.set('view engine', 'ejs');  // Let Express know we want to use EJS.
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/', function (request, response) {
+  response.render('index');
+})
+
+app.use(express.static('public'));  //tell Express that well keep files in the /public directory
+
+
+app.post('/team', function(request, response){
+  let responses = request.body
+
+  //open the data.json file, and parse into a JSON Object
+  var rawFile = fs.readFileSync('data.json')
+  console.log("rawFile:", rawFile.length);
+  if (rawFile.length != 0){
+    var teamUpdates = JSON.parse(rawFile)
+  }
+  else {
+    var teamUpdates = [{}]
+  }
+
+
+  teamUpdates.push(responses)
+
+  //time to save the data back to disk
+  fs.writeFileSync('data.json', JSON.stringify(teamUpdates))
+  response.render('team', responses)
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!')
+})
